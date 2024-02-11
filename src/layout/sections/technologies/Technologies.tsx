e5db8b0d2048269474b4ce63b6d94e53eb07e04a
apastan <apastan.dev@gmail.com>
2024-02-11 23:56:11 +0300

    Create Technologies component

---
+++ src/layout/sections/technologies/Technologies.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexContainer} from "../../../components/styled/FlexContainer";
+
+export function Technologies() {
+    return (
+        <StyledTechnologies>
+            <h2>My Tech Stack</h2>
+            <h3>Technologies I’ve been working with recently</h3>
+            <FlexContainer>
+                {/*  TODO - Create Technology component  */}
+            </FlexContainer>
+        </StyledTechnologies>
+    );
+}
+
+const StyledTechnologies = styled.section`
+
+`
