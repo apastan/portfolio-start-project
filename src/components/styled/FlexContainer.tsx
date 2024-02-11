fff4ec19f3234ea17a0a4a0f8920a9313fc39576
apastan <apastan.dev@gmail.com>
2024-02-11 18:52:42 +0300

    Create FlexContainer styled component

---
+++ src/components/styled/FlexContainer.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+type FlexContainerPropsType = {
+    flexDirection?: "row" | "row-reverse" | "column" | "column-reverse"
+    flexWrap?: "nowrap" | "wrap" | "wrap-reverse"
+    justifyContent?: "flex-start" | "flex-end" | "center" | "space-between" | "space-around"
+    alignItems?: "flex-start" | "flex-end" | "center" | "baseline" | "stretch"
+    alignContent?: "stretch" | "flex-start" | "flex-end"
+}
+
+export const FlexContainer = styled.div<FlexContainerPropsType>`
+    display: flex;
+    flex-direction: ${props => (props.flexDirection ?? "row")};
+    flex-wrap: ${props => (props.flexWrap ?? "nowrap")};
+    justify-content: ${props => (props.justifyContent ?? "flex-start")};
+    align-items: ${props => (props.alignItems ?? "flex-start")};
+    align-content: ${props => (props.alignContent ?? "stretch")};
+`
