d5f5cfcfb4b47884e9f9c555453c5a81b89f1ac1
apastan <apastan.dev@gmail.com>
2024-02-11 19:00:47 +0300

    Use FlexContainer for Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import './App.css';
 import styled from 'styled-components';
 import {Header} from "./layout/header/Header";
+import {Typography} from "./components/styled/Typography";
 
 
 function App() {
     return (
         <>
             <Header/>
+            <Typography as={"h1"} sx={()=> `color: red;`}>
+                Hi 👋,
+                My name is
+                <span>Pavan MG</span>
+                I build things for web
+            </Typography>
         </>
     );
 }
 
 export default App;
 
+/*
+const MainHeading = styled(Typography)`
+    color: aqua;
+`
+*/
+
+/*
 const Title = styled.h1`
     font-size: 1.5em;
     text-align: center;
     color: #e91e63;
-`;
+`;*/
