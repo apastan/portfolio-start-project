1dfa734b3ec9b73f3981dc93963086243ee2b671
apastan <apastan.dev@gmail.com>
2024-02-13 00:34:18 +0300

    Add FlexContainer in Link Component

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode} from 'react';
 import styled from "styled-components";
 import {Icon, IconPropsType} from "../icon/Icon";
+import {FlexContainer} from "../styled/FlexContainer";
 
 type LinkPropsType = {
     iconSettings?: IconPropsType
@@ -11,14 +12,16 @@ type LinkPropsType = {
 export function Link({children, iconSettings, href}: LinkPropsType) {
     return (
         <StyledLink href={href}>
-            <>
+            <FlexContainer justifyContent={"center"} alignItems={"center"}>
                 {iconSettings && <Icon {...iconSettings}></Icon>}
                 {children && <span>{children}</span>}
-            </>
+            </FlexContainer>
         </StyledLink>
     );
 }
 
 const StyledLink = styled.a`
-    
+    svg + span {
+        margin-left: 10px;
+    }
 `
