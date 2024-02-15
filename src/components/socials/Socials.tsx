1c555484eaf1fc58880d3233e805eacd020f79b5
apastan <apastan.dev@gmail.com>
2024-02-15 21:54:48 +0300

    Refactor Socials components

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from "styled-components";
+import {Link} from "../link/Link";
 
 
 export function Socials() {
     return (
         <StyledSocials>
             {/*TODO - Add links to socials*/}
-            <li><a href="#"></a>Icon 1</li>
-            <li><a href="#"></a>Icon 2</li>
-            <li><a href="#"></a>Icon 3</li>
+            <li>
+                <Link href="#">Icon 1</Link>
+            </li>
+            <li>
+                <Link href="#">Icon 2</Link>
+            </li>
+            <li>
+                <Link href="#">Icon 3</Link>
+            </li>
         </StyledSocials>
     );
 }
