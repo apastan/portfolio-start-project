602d01557f6a920238119ce80608e0723a83d859
apastan <apastan.dev@gmail.com>
2024-02-15 23:41:42 +0300

    Styling Menu component

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {useTheme} from "styled-components";
 import {Link} from "../link/Link";
 
 
 export function Socials() {
+    const theme = useTheme();
+
     return (
         <StyledSocials>
             {/*TODO - Add links to socials*/}
             <li>
-                <Link href="#">Icon 1</Link>
+                <Link href="#" iconSettings={{
+                    iconId: "githubHeader",
+                    width: "30px",
+                    height: "30px",
+                    viewBox: "0 0 30 30",
+                    // @ts-ignore
+                    fill: theme.colors.socialIconsHeader
+                }}></Link>
             </li>
             <li>
-                <Link href="#">Icon 2</Link>
+                <Link href="#" iconSettings={{
+                    iconId: "twitterHeader",
+                    width: "30px",
+                    height: "30px",
+                    viewBox: "0 0 32 32",
+                    // @ts-ignore
+                    fill: theme.colors.socialIconsHeader
+                }}></Link>
             </li>
             <li>
-                <Link href="#">Icon 3</Link>
+                <Link href="#" iconSettings={{
+                    iconId: "linkedinHeader",
+                    width: "30px",
+                    height: "30px",
+                    viewBox: "0 0 30 30",
+                    // @ts-ignore
+                    fill: theme.colors.socialIconsHeader
+                }}></Link>
             </li>
         </StyledSocials>
     );
