3bda95bacefdcd6d85e8bd0aaa4982a86646c0d7
apastan <apastan.dev@gmail.com>
2024-02-15 20:39:17 +0300

    Add ThemeProvider component

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {GlobalStyles} from "./styles/Global.styled";
+import {ThemeProvider} from "styled-components";
+import {lightTheme} from "./styles/theme/modes";
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <GlobalStyles/>
-        <App/>
+        <ThemeProvider theme={lightTheme}>
+            <GlobalStyles/>
+            <App/>
+        </ThemeProvider>
     </React.StrictMode>
 );
 
