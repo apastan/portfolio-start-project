eb6bb73b9f48482b08ed70b61774ee47cb82f231
apastan <apastan.dev@gmail.com>
2024-02-15 16:44:30 +0300

    Create component to manage the width of App

---
+++ src/components/styled/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1234px;
+    width: 100%;
+    min-height: 100%;
+    padding: 0 15px;
+    margin: 0 auto;
+    // TODO - Remove border
+    border: 1px solid red;
+`
