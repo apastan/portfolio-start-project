c04d6566efd3e3ea5f0ecd469527a1195118a767
apastan <apastan.dev@gmail.com>
2024-02-16 12:38:59 +0300

    Change mock avatar image (without border)

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-import photo from "../../../assets/images/50353683 1 (1).png"
+import photo from "../../../assets/images/avatar.jpg"
 import {FlexContainer} from "../../../components/styled/FlexContainer";
 
 export function Main() {
