1ca4261a69d36dde1f378ef560f4cfde9a9df8b8
apastan <apastan.dev@gmail.com>
2024-02-18 22:24:35 +0300

    Styling h2 and h3 headings

--- src/components/styled/SectionSubtitle.tsx
+++ src/components/styled/SectionSubtitle.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const SectionSubtitle = styled.h2`
-    color: #666666;
+    font-size: 32px;
+    font-weight: 400;
+    color: ${props => props.theme.colors.h3};
     text-align: center;
+    
+    margin-bottom: 110px;
 `
--- src/components/styled/SectionTitle.tsx
+++ src/components/styled/SectionTitle.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const SectionTitle = styled.h2`
-    color: #42446E;
+    font-size: 48px;
+    font-weight: 700;
+    color: ${props => props.theme.colors.h2};
     text-align: center;
+    
+    margin-bottom: 50px;
 `
