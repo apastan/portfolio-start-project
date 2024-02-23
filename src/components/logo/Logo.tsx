7963724af4aa0487730b49eeb9a5b626528bcff3
apastan <apastan.dev@gmail.com>
2024-02-24 00:27:32 +0300

    Add aria attributes to Logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,9 +4,9 @@ import logo from "../../assets/images/logo.png";
 
 export function Logo() {
     return (
-        <StyledLogo href="#">
+        <StyledLogo href="#" aria-label="Homepage">
             {/* TODO - insert real logo */}
-            <img src={logo} alt="Logo" width="97px" height="59px"/>
+            <img src={logo} alt="Logo" width="97px" height="59px" aria-hidden="true"/>
         </StyledLogo>
     );
 }
